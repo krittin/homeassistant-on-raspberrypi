1127cc5d87246e7e0837c5fb73bc4366eaa3b57f
krittin <krittin.tt@gmail.com>
2022-09-07 21:17:18 +0100

    updated deploy.sh for the whole installation, updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# k3s-on-raspberrypi
+# Home Assistant on Raspberry Pi
+
+Home Assistant deployed on K3S with Longhorn, all of these running on a 4 Raspberry Pi cluster
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
 time ansible-playbook -i inventory/pi k3s.yml
+time ansible-playbook -i inventory/pi metallb.yml
+time ansible-playbook -i inventory/pi longhorn.yml
+time ansible-playbook -i inventory/pi home-assistant.yml
